a31df8296321d6b2b66a981f63aed877544da973
Valentinka2222 <xamalyan89@gmail.com>
2022-10-25 20:40:35 +0300

    2

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -2,16 +2,11 @@ import React, { useState } from 'react';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteSharpIcon from '@mui/icons-material/FavoriteSharp';
 import { setFavourite } from '../../store';
-import { FixedList, ProductInt } from '../../interfaces';
+import { FixedList } from '../../interfaces';
 
 const Product: React.FC<FixedList> = ({ style, data, rowIndex, itemPer, columnIndex }) => {
   const [isLike, setIsLike] = useState(false);
   const itemInd = Math.ceil(rowIndex * itemPer + columnIndex);
-  const changeColor = (el?: ProductInt, like?: boolean) => {
-    if (like) {
-      setFavourite(el);
-    }
-  };
 
   const handleClick = (e: React.SyntheticEvent<EventTarget>) => {
     if (!(e.target instanceof HTMLDivElement)) {
@@ -19,8 +14,7 @@ const Product: React.FC<FixedList> = ({ style, data, rowIndex, itemPer, columnIn
     }
     const eventId = e.target.dataset.id;
     const dataElement = data.find(el => el.id === Number(eventId));
-
-    changeColor(dataElement, isLike);
+    setFavourite(dataElement);
   };
   const { id, src, name, price } = data[itemInd];
 
@@ -36,9 +30,7 @@ const Product: React.FC<FixedList> = ({ style, data, rowIndex, itemPer, columnIn
 
           <button
             aria-controls="icon"
-            onClick={() => {
-              setIsLike(true);
-            }}
+            onClick={() => setIsLike(true)}
             aria-haspopup="true"
             data-id={id}
             className="icon-like"
